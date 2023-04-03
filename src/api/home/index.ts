17e8e6f8ddab82946dd4d801a2459286fcbd36e9
cuonghm <cuonghm@unikom.vn>
2023-04-03 22:42:54 +0700

    fix img object fit

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -4,7 +4,7 @@ import { instance } from '../axios';
 export const getBannerHome = async () => {
   return [
     'https://jdsportsusablog.s3.amazonaws.com/wp-content/uploads/2023/01/Ja1-HeaderBanner-01.jpg',
-    'https://e0.pxfuel.com/wallpapers/536/1007/desktop-wallpaper-basketball-shoes-sf-shoes-shop-banner-design-nike-basketball-shoes.jpg',
-    'https://www.si.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_xy_center%2Cq_auto:good%2Cw_1200%2Cx_2357%2Cy_2273/MTk0ODgwMzM1NDQ2NTQ5NjY2/usatsi_19706694.jpg',
+    'https://myshoes.vn/image/cache/catalog/2022/banner/cata/giay-nike-nu-chinh-hang-600x315h.png',
+    'https://intphcm.com/data/upload/poster-giay-ad.jpg',
   ];
 };
--- src/components/home/BannerDetail.vue
+++ src/components/home/BannerDetail.vue
@@ -1,53 +1,24 @@
 <template>
   <div class="mt-3">
     <div class="row">
-      <div class="col-4">
+      <div
+        class="col-4"
+        v-for="item in data"
+        :key="item.url"
+        ref="BannerProds"
+      >
         <div class="card">
           <img
             class="card-img-top"
-            src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmlrZSUyMHNob2VzfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+            :src="item.url"
             alt="Card image cap"
           />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card
-              title and make up the bulk of the card's
-              content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmlrZSUyMHNob2VzfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card
-              title and make up the bulk of the card's
-              content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmlrZSUyMHNob2VzfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card
-              title and make up the bulk of the card's
-              content.
+            <h5 class="card-title">
+              {{ item.title }}
+            </h5>
+            <p class="card-text text-start">
+              {{ item.description }}
             </p>
           </div>
         </div>
@@ -65,4 +36,77 @@
     text-align: center;
   }
 }
+
+.card-img-top {
+  height: 350px;
+  object-fit: cover;
+  border-radius: 8px;
+}
 </style>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, watch } from 'vue';
+import ScrollReveal from 'scrollreveal';
+
+export default defineComponent({
+  setup() {
+    const BannerProds = ref<any>();
+
+    const data = reactive([
+      {
+        url: 'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1824,c_limit/77dbf580-d3e9-4db0-8901-72fab0bcbdba/nike-just-do-it.png',
+        title:
+          'Hướng dẫn cá nhân của bạn để trở nên vĩ đại',
+        description: `Ứng dụng Nike có mọi thứ bạn cần để di chuyển.
+              Điều đó có nghĩa là thiết bị mới nhất, câu
+              chuyện hấp dẫn và cộng đồng trên toàn thế
+              giới. Tất cả đều ở đây, được cá nhân hóa cho
+              bạn.`,
+      },
+      {
+        url: 'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_1824,c_limit/b38f0b02-991b-4ed6-8398-8d619e5d9b9a/nike-app.png',
+        title: 'Tham gia vào cộng đồng',
+        description: `Khi bạn ở trong ứng dụng, bạn là Thành viên
+              của Nike. Vì vậy, bạn sẽ có quyền truy cập đầu
+              tiên vào thiết bị mới nhất - cộng với một chỗ
+              ngồi ở hàng ghế đầu để trải nghiệm Nike.`,
+      },
+      {
+        url: 'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_906,c_limit/16e2afe9-7657-4976-8b1f-446fbad97e87/nike-app.jpg',
+        title: 'Tu Luyện Cho Mọi Thân Tâm',
+        description: `Ứng dụng Nike Training Club sẽ giúp thói quen
+              tập thể dục của bạn trở nên gắn bó với các tùy
+              chọn nhanh hơn, công cụ đặt mục tiêu và nội
+              dung mới hàng ngày. Tăng cường tư duy và cơ
+              bắp của bạn với sự hướng dẫn miễn phí từ các
+              huấn luyện viên, vận động viên và chuyên gia
+              chăm sóc sức khỏe yêu thích của bạn.`,
+      },
+    ]);
+
+    watch(
+      () => BannerProds.value,
+      () => {
+        if (BannerProds.value[0]) {
+          BannerProds.value.forEach(
+            (element: any, index: number) => {
+              ScrollReveal().reveal(element, {
+                delay: 200 * index,
+                reset: true,
+                duration: 1000,
+                distance: '150px',
+                opacity: 0,
+                origin: 'right',
+              });
+            }
+          );
+        }
+      }
+    );
+    return {
+      data,
+      BannerProds,
+    };
+  },
+});
+</script>
